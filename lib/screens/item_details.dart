fe56583779d25454159c720836efc2ab94bfa392
oussama aziz <oaziz615@gmail.com>
2021-10-24 17:39:51 +0200

    added responsive design 2

--- lib/screens/item_details.dart
+++ lib/screens/item_details.dart
@@ -1,3 +1,4 @@
+import 'package:ecomerce_store1/helpers/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,15 +17,18 @@ class ItemDetails extends StatelessWidget {
           Stack(
             children: [
               Container(
-                height: 690,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                height: 86 * SizeConfig.heightMultiplier,
+                padding: EdgeInsets.symmetric(
+                    vertical: 3.7 * SizeConfig.heightMultiplier,
+                    horizontal: 5 * SizeConfig.widthMultiplier),
                 alignment: Alignment.bottomLeft,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
+                    bottomLeft:
+                        Radius.circular(3.7 * SizeConfig.heightMultiplier),
+                    bottomRight:
+                        Radius.circular(3.7 * SizeConfig.heightMultiplier),
                   ),
                 ),
                 child: Column(
@@ -44,8 +48,8 @@ class ItemDetails extends StatelessWidget {
                                   fontWeight: FontWeight.bold),
                         ),
                         Container(
-                          height: 50,
-                          width: 50,
+                          height: 6.3 * SizeConfig.heightMultiplier,
+                          width: 13 * SizeConfig.widthMultiplier,
                           decoration: const BoxDecoration(
                             shape: BoxShape.circle,
                             color: Color(0xffFF8A00),
@@ -64,27 +68,24 @@ class ItemDetails extends StatelessWidget {
                             fontWeight: FontWeight.bold,
                           ),
                     ),
-                    const SizedBox(height: 5),
+                    SizedBox(height: 0.6 * SizeConfig.heightMultiplier),
                     Text(
                       'These garments contain recycled fiber drawn from our in-house productions, meanings we transform waste into a resource .',
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption!
-                          .copyWith(color: Colors.black, fontSize: 15),
+                      style: Theme.of(context).textTheme.caption!.copyWith(
+                          color: Colors.black,
+                          fontSize: 1.9 * SizeConfig.heightMultiplier),
                     ),
-                    const SizedBox(height: 15),
+                    SizedBox(height: 1.9 * SizeConfig.heightMultiplier),
                     Text(
                       'COLOR',
-                      style: Theme.of(context)
-                          .textTheme
-                          .caption!
-                          .copyWith(fontSize: 17),
+                      style: Theme.of(context).textTheme.caption!.copyWith(
+                          fontSize: 2.2 * SizeConfig.heightMultiplier),
                     ),
                     Row(
                       children: [
                         Container(
-                          height: 45,
-                          width: 45,
+                          height: 5.6 * SizeConfig.heightMultiplier,
+                          width: 11.5 * SizeConfig.widthMultiplier,
                           decoration: const BoxDecoration(
                               shape: BoxShape.circle, color: Color(0xff2D3B5A)),
                           child: const Icon(
@@ -92,31 +93,31 @@ class ItemDetails extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
-                        const SizedBox(width: 10),
+                        SizedBox(width: 2.6 * SizeConfig.widthMultiplier),
                         Container(
-                          height: 45,
-                          width: 45,
+                          height: 5.6 * SizeConfig.heightMultiplier,
+                          width: 11.5 * SizeConfig.widthMultiplier,
                           decoration: const BoxDecoration(
                               shape: BoxShape.circle, color: Color(0xffFF3F73)),
                         ),
-                        const SizedBox(width: 10),
+                        SizedBox(width: 2.6 * SizeConfig.widthMultiplier),
                         Container(
-                          height: 45,
-                          width: 45,
+                          height: 5.6 * SizeConfig.heightMultiplier,
+                          width: 11.5 * SizeConfig.widthMultiplier,
                           decoration: const BoxDecoration(
                               shape: BoxShape.circle, color: Color(0xffFF8B01)),
                         ),
-                        const SizedBox(width: 10),
+                        SizedBox(width: 2.6 * SizeConfig.widthMultiplier),
                         Container(
-                          height: 45,
-                          width: 45,
+                          height: 5.6 * SizeConfig.heightMultiplier,
+                          width: 11.5 * SizeConfig.widthMultiplier,
                           decoration: const BoxDecoration(
                               shape: BoxShape.circle, color: Color(0xff693ADE)),
                         ),
-                        const SizedBox(width: 10),
+                        SizedBox(width: 2.6 * SizeConfig.widthMultiplier),
                         Container(
-                          height: 45,
-                          width: 45,
+                          height: 5.6 * SizeConfig.heightMultiplier,
+                          width: 11.5 * SizeConfig.widthMultiplier,
                           decoration: const BoxDecoration(
                               shape: BoxShape.circle, color: Color(0xffDADADA)),
                         ),
@@ -126,15 +127,17 @@ class ItemDetails extends StatelessWidget {
                 ),
               ),
               Container(
-                height: 400,
+                height: 50 * SizeConfig.heightMultiplier,
                 width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                  color: Color(0xffECECF0),
+                decoration: BoxDecoration(
+                  color: const Color(0xffECECF0),
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
+                    bottomLeft:
+                        Radius.circular(5 * SizeConfig.heightMultiplier),
+                    bottomRight:
+                        Radius.circular(5 * SizeConfig.heightMultiplier),
                   ),
-                  boxShadow: [
+                  boxShadow: const [
                     BoxShadow(
                       color: Colors.black45,
                       offset: Offset(0, 3),
@@ -171,25 +174,25 @@ class ItemDetails extends StatelessWidget {
                     ),
                     Positioned(
                       child: Row(
-                        children: const [
+                        children: [
                           Icon(
                             Icons.circle_rounded,
-                            size: 15,
+                            size: 2 * SizeConfig.heightMultiplier,
                             color: Colors.white,
                           ),
                           Icon(
                             Icons.circle_outlined,
-                            size: 15,
+                            size: 2 * SizeConfig.heightMultiplier,
                             color: Colors.white,
                           ),
                           Icon(
                             Icons.circle_outlined,
-                            size: 15,
+                            size: 2 * SizeConfig.heightMultiplier,
                             color: Colors.white,
                           ),
                           Icon(
                             Icons.circle_outlined,
-                            size: 15,
+                            size: 2 * SizeConfig.heightMultiplier,
                             color: Colors.white,
                           ),
                         ],
@@ -202,69 +205,75 @@ class ItemDetails extends StatelessWidget {
             ],
           ),
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            padding: EdgeInsets.symmetric(
+                vertical: 2.5 * SizeConfig.heightMultiplier,
+                horizontal: 5 * SizeConfig.widthMultiplier),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
+                Text(
                   'RECOMMEND',
                   style: TextStyle(
-                    color: Color(0xff545B74),
-                    fontSize: 15,
+                    color: const Color(0xff545B74),
+                    fontSize: 1.9 * SizeConfig.heightMultiplier,
                   ),
                 ),
-                const SizedBox(height: 10),
+                SizedBox(height: 1.3 * SizeConfig.heightMultiplier),
                 Row(
                   children: [
                     Container(
-                      height: 40,
-                      width: 40,
+                      height: 5 * SizeConfig.heightMultiplier,
+                      width: 10.2 * SizeConfig.widthMultiplier,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(13),
+                        borderRadius: BorderRadius.circular(
+                            1.6 * SizeConfig.heightMultiplier),
                         image: const DecorationImage(
                             image: AssetImage('assets/belted_dress.jpg'),
                             fit: BoxFit.fitWidth),
                       ),
                     ),
-                    const SizedBox(width: 5),
+                    SizedBox(width: 1.3 * SizeConfig.widthMultiplier),
                     Container(
-                      height: 40,
-                      width: 40,
+                      height: 5 * SizeConfig.heightMultiplier,
+                      width: 10.2 * SizeConfig.widthMultiplier,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(13),
+                        borderRadius: BorderRadius.circular(
+                            1.6 * SizeConfig.heightMultiplier),
                         image: const DecorationImage(
                             image: AssetImage('assets/floral_dress.jpg'),
                             fit: BoxFit.fitWidth),
                       ),
                     ),
-                    const SizedBox(width: 5),
+                    SizedBox(width: 1.3 * SizeConfig.widthMultiplier),
                     Container(
-                      height: 40,
-                      width: 40,
+                      height: 5 * SizeConfig.heightMultiplier,
+                      width: 10.2 * SizeConfig.widthMultiplier,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(13),
+                        borderRadius: BorderRadius.circular(
+                            1.6 * SizeConfig.heightMultiplier),
                         image: const DecorationImage(
                             image: AssetImage('assets/mini_dress.jpg'),
                             fit: BoxFit.fitWidth),
                       ),
                     ),
-                    const SizedBox(width: 5),
+                    SizedBox(width: 1.3 * SizeConfig.widthMultiplier),
                     Container(
-                      height: 40,
-                      width: 40,
+                      height: 5 * SizeConfig.heightMultiplier,
+                      width: 10.2 * SizeConfig.widthMultiplier,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(13),
+                        borderRadius: BorderRadius.circular(
+                            1.6 * SizeConfig.heightMultiplier),
                         image: const DecorationImage(
                             image: AssetImage('assets/contrasting_satin.jpg'),
                             fit: BoxFit.fitWidth),
                       ),
                     ),
-                    const SizedBox(width: 15),
-                    const Text(
+                    SizedBox(width: 3.8 * SizeConfig.widthMultiplier),
+                    Text(
                       '+32 more',
                       style: TextStyle(
-                        color: Color(0xff545B74),
-                        fontSize: 12,
+                        color: const Color(0xff545B74),
+                        fontSize: 1.5 * SizeConfig.heightMultiplier,
                       ),
                     ),
                   ],
